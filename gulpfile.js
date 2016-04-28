627ea5c634bc5cc8a6d7487228e568cf10192c09
Leonard Lamprecht <mindrun@icloud.com>
2016-04-28 13:55:02 +0200

    Clean up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,37 @@
-const gulp = require('gulp');
-const del = require('del');
-const ava = require('gulp-ava');
-const babel = require('gulp-babel');
-const cache = require('gulp-cached');
-const eslint = require('gulp-eslint');
-const help = require('gulp-task-listing');
+const gulp = require('gulp')
+const del = require('del')
+const cache = require('gulp-cached')
+const eslint = require('gulp-eslint')
+const help = require('gulp-task-listing')
+const ava = require('gulp-ava')
+const babel = require('gulp-babel')
 
-gulp.task('help', help);
+gulp.task('help', help)
 
 gulp.task('compile', [
   'compile-bin',
   'compile-test'
-]);
+])
 
-gulp.task('compile-bin', function () {
+gulp.task('compile-bin', () => {
   return gulp.src('bin/*')
-  .pipe(babel({
-    presets: ['es2015']
-  }))
-  .pipe(gulp.dest('build/bin'));
+  .pipe(babel())
+  .pipe(gulp.dest('build/bin'))
 });
 
-gulp.task('compile-test', function () {
+gulp.task('compile-test', () => {
   return gulp.src('test/*.js')
   .pipe(cache('test'))
-  .pipe(babel({
-    presets: ['es2015'],
-    plugins: [
-      'transform-runtime',
-      'syntax-async-functions',
-      'transform-async-to-generator'
-    ]
-  }))
-  .pipe(gulp.dest('build/test'));
-});
+  .pipe(babel())
+  .pipe(gulp.dest('build/test'))
+})
 
-gulp.task('test', ['compile'], function () {
+gulp.task('test', ['compile'], () => {
   return gulp.src('build/test/*.js')
-  .pipe(ava());
-});
+  .pipe(ava())
+})
 
-gulp.task('lint', function () {
+gulp.task('lint', () => {
   return gulp.src([
     'gulpfile.js',
     'test/*.js',
@@ -48,11 +39,8 @@ gulp.task('lint', function () {
   ])
   .pipe(eslint())
   .pipe(eslint.format())
-  .pipe(eslint.failAfterError());
-});
+  .pipe(eslint.failAfterError())
+})
 
-gulp.task('clean', function () {
-  return del(['build']);
-});
-
-gulp.task('default', ['lint', 'compile', 'test']);
+gulp.task('clean', () => del(['build']))
+gulp.task('default', ['lint', 'compile', 'test'])
