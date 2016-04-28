d56edae1b9beb0e6143620ae20fed6359b652ed9
Leonard Lamprecht <mindrun@icloud.com>
2016-04-28 14:29:30 +0200

    Unncessary stuff removed

--- test/_listen.js
+++ test/_listen.js
@@ -1,9 +1,10 @@
 import micro from 'micro-serve'
 
-export default async function listen (fn, opts) {
+export default async function (fn, opts) {
   const srv = micro(fn, opts)
+
   return new Promise((resolve, reject) => {
-    srv.listen((err) => {
+    srv.listen(err => {
       if (err) return reject(err)
       const { port } = srv.address()
       resolve(`http://localhost:${port}`)
