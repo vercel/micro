11cfb1dd1a973d4db261214d2e009d9c775bb596
Guillermo Rauch <rauchg@gmail.com>
2016-04-28 10:45:47 -0700

    get rid of big babel-runtime

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp')
 const babel = require('gulp-babel')
 const cache = require('gulp-cached')
+const concat = require('gulp-concat-util');
 
 const path = 'lib/**/*'
 
@@ -8,6 +9,7 @@ gulp.task('transpile', () => {
   return gulp.src(path)
   .pipe(cache('transpile'))
   .pipe(babel())
+  .pipe(concat.header('var regeneratorRuntime = require(\'babel-regenerator-runtime\');'))
   .pipe(gulp.dest('dist'))
 })
 
