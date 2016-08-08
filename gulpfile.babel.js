081c4d21cbc1b244d0e151bd9933bf27fec49338
Leo Lamprecht <mindrun@icloud.com>
2016-08-08 11:14:14 +0200

    Use next gen code for gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,17 +1,15 @@
-const gulp = require('gulp')
-const babel = require('gulp-babel')
-const cache = require('gulp-cached')
-const concat = require('gulp-concat-util')
+import gulp from 'gulp'
+import babel from 'gulp-babel'
+import cache from 'gulp-cached'
+import concat from 'gulp-concat-util'
 
 const path = 'lib/**/*'
 
-gulp.task('transpile', () => {
-  return gulp.src(path)
+gulp.task('transpile', () => gulp.src(path)
   .pipe(cache('transpile'))
   .pipe(babel())
   .pipe(concat.header('var regeneratorRuntime = require(\'babel-regenerator-runtime\');'))
-  .pipe(gulp.dest('dist'))
-})
+  .pipe(gulp.dest('dist')))
 
 gulp.task('watch', () => {
   gulp.watch(path, ['transpile'])
