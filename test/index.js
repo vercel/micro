vercel/micro

commit 786ee1ad7e324825cee16ad38dfeeaee8246ad8f

Remove bluebird.coroutine (#163)
* remove bluebird coroutine

* use bluebird Promise.resolve

lib/server.js
@@ -7,14 +7,12 @@ const typer = require('media-typer')
 const isStream = require('isstream')
 const Promise = require('bluebird')
 
-const {coroutine, resolve} = Promise
+const {resolve} = Promise
 
 const DEV = process.env.NODE_ENV === 'development'
 const TESTING = process.env.NODE_ENV === 'test'
 
-const serve = fn => server(coroutine(function * (req, res) {
-  yield exports.run(req, res, fn)
-}))
+const serve = fn => server((req, res) => exports.run(req, res, fn))
 
 module.exports = exports = serve
 
@@ -22,22 +20,19 @@ exports.send = send
 exports.sendError = sendError
 exports.createError = createError
 
-exports.run = coroutine(function * (req, res, fn) {
-  try {
-    const val = yield resolve(fn(req, res))
+exports.run = (req, res, fn) =>
+  resolve(fn(req, res))
+    .then(val => {
+      if (val === null) {
+        send(res, 204, null)
+      }
 
-    if (val === null) {
-      send(res, 204, null)
-    }
-
-    // Return a undefined-null value -> send
-    if (undefined !== val) {
-      send(res, res.statusCode || 200, val)
-    }
-  } catch (err) {
-    sendError(req, res, err)
-  }
-})
+      // Return a undefined-null value -> send
+      if (undefined !== val) {
+        send(res, res.statusCode || 200, val)
+      }
+    })
+    .catch(err => sendError(req, res, err))
 
 // maps requests to buffered raw bodies so that
 // multiple calls to `json` work as expected

commit 5bd97b9d4acdf0626f117c51184392ee6c1bd00e

throwing in sync function should work
fixing regression in #163

lib/server.js
@@ -5,7 +5,7 @@ const server = require('http').Server
 const getRawBody = require('raw-body')
 const typer = require('media-typer')
 const isStream = require('isstream')
-const {resolve} = require('bluebird')
+const Promise = require('bluebird')
 
 const DEV = process.env.NODE_ENV === 'development'
 const TESTING = process.env.NODE_ENV === 'test'
@@ -19,7 +19,7 @@ exports.sendError = sendError
 exports.createError = createError
 
 exports.run = (req, res, fn) =>
-  resolve(fn(req, res))
+  new Promise(resolve => resolve(fn(req, res)))
     .then(val => {
       if (val === null || val === undefined) {
         send(res, 204, null)
@@ -42,7 +42,7 @@ const parseJSON = str => {
   }
 }
 
-exports.json = (req, {limit = '1mb'} = {}) => resolve().then(() => {
+exports.json = (req, {limit = '1mb'} = {}) => Promise.resolve().then(() => {
   const type = req.headers['content-type']
   const length = req.headers['content-length']
   const encoding = typer.parse(type).parameters.charset

test/index.js
@@ -243,6 +243,20 @@ test('throw (500)', async t => {
   }
 })
 
+test('throw (500) sync', async t => {
+  const fn = () => {
+    throw new Error('500 from test (expected)')
+  }
+
+  const url = await getUrl(fn)
+
+  try {
+    await request(url)
+  } catch (err) {
+    t.deepEqual(err.statusCode, 500)
+  }
+})
+
 test('send(200, <Stream>) with error on same tick', async t => {
   const fn = async (req, res) => {
     const stream = resumer().queue('error-stream')