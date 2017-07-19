87da2939965ac02ca5075c57ad59d733ee69ee62
Leo Lamprecht <mindrun@icloud.com>
2017-07-19 19:22:46 +0200

    Break down the module usage

--- bin/micro.js
+++ bin/micro.js
@@ -4,30 +4,15 @@
 const path = require('path')
 
 // Packages
-const updateNotifier = require('update-notifier')
-const nodeVersion = require('node-version')
 const args = require('args')
-const isAsyncSupported = require('is-async-supported')
-
-// Ours
-const pkg = require('../package')
-
-// Throw an error if node version is too low
-if (nodeVersion.major < 6) {
-  console.error(
-    `Error! Micro requires at least version 6 of Node. Please upgrade!`
-  )
-  process.exit(1)
-}
-
-// Let user know if there's an update
-// This isn't important when deployed to Now
-if (!process.env.NOW && pkg.dist) {
-  updateNotifier({ pkg }).notify()
-}
 
 args
-  .option('port', 'Port to listen on', parseInt(process.env.PORT, 10) || 3000, Number)
+  .option(
+    'port',
+    'Port to listen on',
+    parseInt(process.env.PORT, 10) || 3000,
+    Number
+  )
   .option(['H', 'host'], 'Host to listen on', '0.0.0.0')
   .option(['s', 'silent'], 'Silent mode')
 
@@ -60,31 +45,10 @@ if (!file) {
 
 if (!file) {
   console.error('micro: Please supply a file.')
-  args.showHelp()
 }
 
 if (file[0] !== '/') {
   file = path.resolve(process.cwd(), file)
 }
 
-if (!isAsyncSupported()) {
-  const asyncToGen = require('async-to-gen/register')
-  // Support for keywords "async" and "await"
-  const pathSep = process.platform === 'win32' ? '\\\\' : '/'
-  const directoryName = path.parse(path.join(__dirname, '..')).base
-
-  // This is required to make transpilation work on Windows
-  const fileDirectoryPath = path.parse(file).dir.split(path.sep).join(pathSep)
-
-  asyncToGen({
-    includes: new RegExp(
-      `.*${directoryName}?${pathSep}(lib|bin)|${fileDirectoryPath}.*`
-    ),
-    excludes: null,
-    sourceMaps: false
-  })
-}
-
-// Load package core with async/await support
-// If needed... Otherwise use the native implementation
 require('../lib')(file, flags)
