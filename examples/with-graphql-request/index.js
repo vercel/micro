e34c709c1a4ac8a8de4562e82a605ddb20ae7fbb
pex <rernst@farbenmeer.net>
2017-07-26 13:58:46 +0200

    Add graphql-request example

---
+++ examples/with-graphql-request/index.js
@@ -0,0 +1,22 @@
+const { GraphQLClient, request } = require('graphql-request')
+const endpoint = 'https://api.graph.cool/simple/v1/movies'
+
+// Prepare simple query
+const query = `
+  query Movie($title: String!) {
+    movie: Movie(title: $title) {
+      releaseDate
+      actors {
+        name
+      }
+    }
+  }
+`
+
+module.exports = async function (req, res) {
+  // Perform query
+  const data = await request(endpoint, query, { title: 'Inception' })
+
+  // Return Movie
+  return data.movie
+}
