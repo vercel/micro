f8b7b4165f64275728c43b10a93a3ab6e7b2f3b1
Luke Edwards <luke.edwards05@gmail.com>
2017-08-01 11:22:13 -0700

    replace string-cast w/ defaults + implicit casts
    - `http` uses ‘::’ for ‘host’ if nully

--- bin/micro.js
+++ bin/micro.js
@@ -16,7 +16,10 @@ const logError = require('../lib/error')
 
 // Check if the user defined any options
 const flags = parseArgs(process.argv.slice(2), {
-  string: ['host', 'port'],
+  default: {
+    host: '::',
+    port: 3000
+  },
   alias: {
     p: 'port',
     H: 'host',
@@ -87,7 +90,7 @@ server.on('error', err => {
   process.exit(1)
 })
 
-server.listen(flags.port || 3000, flags.host, () => {
+server.listen(flags.port, flags.host, () => {
   const details = server.address()
 
   process.on('SIGTERM', () => {
