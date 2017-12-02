ab74cb0dd75d6f083ee6e3ff69647195d9208445
Rich Powell <powell.rich@gmail.com>
2017-12-02 08:59:05 -0800

    Update index.js (#330)

--- examples/external-api-call/index.js
+++ examples/external-api-call/index.js
@@ -4,5 +4,5 @@ module.exports = async function (req, res) {
   const response = await fetch('https://api.example.com')
   const json = await response.json()
 
-  return json.example
+  return json
 }
