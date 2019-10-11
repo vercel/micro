49f14eb657c61fec11bbc382d9fe71553021d1df
Olli Vanhoja <olli.vanhoja@gmail.com>
2019-10-11 20:24:28 +0300

    Remove unused files

--- src/log-error.ts
+++
@@ -1,4 +0,0 @@
-export default function logError(message: string, errorCode: string): void {
-	console.error(`micri: ${message}`);
-	console.error(`micri: https://github.com/OlliV/micri/blob/master/errors/${errorCode}.md`);
-}
