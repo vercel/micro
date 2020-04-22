790c4f4fa9b24d83a4c7ec31fe2305a95aeee743
Olli Vanhoja <olli.vanhoja@gmail.com>
2020-04-22 19:07:05 +0200

    Pass request options to the handler executed in a worker

--- examples/with-worker/src/index.ts
+++ examples/with-worker/src/index.ts
@@ -3,6 +3,7 @@ import { parse } from 'url';
 import micri, {
 	IncomingMessage,
 	ServerResponse,
+	MicriHandler,
 	Router,
 	send,
 	withWorker
@@ -12,10 +13,14 @@ import prime  from './prime';
 const { router, on, otherwise } = Router;
 
 const parsePath = (req: IncomingMessage): string => parse(req.url || '/').path || '/';
+const withCustomOpts = (hndl: MicriHandler): MicriHandler =>
+	(req: IncomingMessage, res: ServerResponse) =>
+		hndl(req, res, { optX: 'Hello world!' });
 
 micri(router(
 	on.get((req: IncomingMessage) => parsePath(req) === '/main', prime),
 	on.get((req: IncomingMessage) => parsePath(req) === '/worker', withWorker(path.join(__dirname, './prime.js'))),
 	on.get((req: IncomingMessage) => parsePath(req) === '/stream', withWorker(path.join(__dirname, './stream.js'))),
+	on.get((req: IncomingMessage) => parsePath(req) === '/opts', withCustomOpts(withWorker(path.join(__dirname, './opts.js')))),
 	otherwise((_req: IncomingMessage, res: ServerResponse) => send(res, 400, 'Method Not Accepted'))))
 	.listen(3000);
---
+++ examples/with-worker/src/opts.ts
@@ -0,0 +1,3 @@
+export default function fn(_req: any, _res: any, { optX }: { optX: string }) {
+	return optX;
+}
