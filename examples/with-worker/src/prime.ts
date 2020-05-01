46fbe624aacf3d0a2d0727c8451838c08a3a021d
Olli Vanhoja <olli.vanhoja@gmail.com>
2020-05-02 00:28:50 +0200

    Use handler-agent for better with-worker performance
    Currently `with-worker` causes `worker-wrapper` to create temp
    HTTP server which is used to serve the request over TCP. This
    naturally causes some unnecessary latency as the data must go thru
    the kernel in order to get it back to the handler. While
    `handler-agent` still creates a temporary server it uses a clever
    way to pipe the request directly to the handler thru Node.js's
    internals, avoiding all the syscalls and random latency penalties.

--- examples/with-worker/src/prime.ts
+++ examples/with-worker/src/prime.ts
@@ -2,6 +2,8 @@ export default function fn() {
 	const limit = 3e5;
 	let r;
 
+	console.log('This is going to take a while...');
+
 	for (let n = 2; n <= limit; n++) {
 		let isPrime = true;
 		for(let factor = 2; factor < n; factor++) {
