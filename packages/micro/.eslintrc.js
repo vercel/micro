264b530d2ce8082ca6425771a8c4895d3b1721d1
Peter Mbanugo <p.mbanugo@yahoo.com>
2022-07-17 11:57:51 +0200

    feat: configure TypeScript and ESLint

---
+++ packages/micro/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  extends: [
+    require.resolve('@vercel/style-guide/eslint/node'),
+    require.resolve('@vercel/style-guide/eslint/typescript'),
+  ],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  ignorePatterns: ['dist/**'],
+};
